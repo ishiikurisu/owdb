71f8ebf1bbd8ae3fac9ecf82ae2a611185447508
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-12-12 18:21:22 -0200

    Google Drive connection setup

--- app/models/spreadsheet.rb
+++ app/models/spreadsheet.rb
@@ -6,8 +6,17 @@ class Spreadsheet
 
     def initialize key
         session = GoogleDrive::Session.from_service_account_key(key)
-        @spreadsheet = session.spreadsheet_by_title("OWDB")
-        @teamsheet = spreadsheet.worksheets.first
-        @playersheet = spreadsheet.worksheets.second
+        spreadsheet = session.spreadsheet_by_title("OWDB")
+        @teamsheet = spreadsheet.worksheets[0].rows
+        @playersheet = spreadsheet.worksheets[1].rows
+    end
+
+    def get_team_ids
+        @teamsheet[1..-1].map { |e| e[0] }
+    end
+
+    def get_players_from_team_id id
+        team_id_field = @playersheet[0].index 'time_id'
+        @playersheet[1..-1].select { |player| player[team_id_field] == id }
     end
 end
--- test/models/test_spreadsheet.rb
+++ test/models/test_spreadsheet.rb
@@ -7,8 +7,13 @@ class TestSpreadsheet < MiniTest::Test
     end
 
     def test_start_session_on_google_drive
-        assert_equal false, true
-        assert_equal 'id', @spreadsheet.teamsheet.rows[0][0]
-        assert_equal 'id', @spreadsheet.playersheet.rows[0][0]
+        assert_equal 'id', @spreadsheet.teamsheet[0][0]
+        assert_equal 'id', @spreadsheet.playersheet[0][0]
+    end
+
+    def test_can_organize_players_on_teams
+        team_ids = @spreadsheet.get_team_ids
+        players = @spreadsheet.get_players_from_team_id team_ids[0]
+        assert_equal 3, players.length
     end
 end
